ef07c6fb01c19f5256a693b22763147cc25fd6f9
Shailesh <shailesh.patel2009@gmail.com>
2016-04-29 01:50:36 +0530

    Clean code of master docker file

--- JenkinsMaster/jenkins_setup.sh
+++ JenkinsMaster/jenkins_setup.sh
@@ -5,13 +5,10 @@ sudo apt-get upgrade
 
 #Install cURL and docker automated setup from Docker site
 sudo apt-get install curl -y
-#curl -fsSL https://get.docker.com/ | sh
 
 #Setup Git and pull Dockerfile
 sudo apt-get install git -y
 
-#git clone https://github.com/michaelneale/jenkins-ci.org-docker.git
-#sudo docker build -t jenkins jenkins-ci.org-docker/
 sudo docker build -t jenkins .
 sudo docker run -v /home/ubuntu/jenkins_home:/var/jenkins_home -p 8080:8080 -d jenkins
 
