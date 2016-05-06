2d81c8f1b81b68b114e8e4c9d4cea2707e270c5e
Shailesh <shailesh.patel2009@gmail.com>
2016-05-06 11:48:23 +0530

    Add shell script to setup Jenkins using Nginx Proxy

---
+++ JenkinsMaster/jenkins_setup_with_nginxproxy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get upgrade
+
+#Install cURL and docker automated setup from Docker site
+sudo apt-get install curl -y
+
+#Setup Git and pull Dockerfile
+sudo apt-get install git -y
+
+# To create image for jenkins version <2
+#sudo docker build -t jenkins .
+
+# To create image for jenkins version >=2.0
+sudo docker build -t jenkins_version_2 .
+
+# To setup old version of jenkins i.e. <2.0
+#sudo docker run -v /home/ubuntu/jenkins_home:/var/jenkins_home -p 8080:8080 -d jenkins
+# TO setup new version of jenkins i.e. >=2.0
+sudo docker run -v /home/ubuntu/jenkins_home:/var/jenkins_home -p 127.0.0.1:8080:8080 -d jenkins_version_2
+
+echo "JENKINS RUNNING LOCALLY(127.0.0.1) ON PORT 8080"
