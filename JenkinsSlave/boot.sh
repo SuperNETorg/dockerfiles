e293e1ad872f2de5fb2a9ab327f6f49de807f490
Shailesh <shailesh.patel2009@gmail.com>
2016-08-17 13:51:03 +0200

    Add android NDK installation steps

--- JenkinsSlave/boot.sh
+++ JenkinsSlave/boot.sh
@@ -21,3 +21,20 @@
  git config --global user.email "flamingice@users.noreply.github.com"
 # ./make_all.sh curl
  NACL_ARCH=pnacl make curl
+
+
+# Download Android NDK 
+cd /home/autobuild && wget http://dl.google.com/android/repository/android-ndk-r12b-linux-x86_64.zip -O android-ndk.zip
+cd /home/autobuild && unzip android-ndk.zip
+cd /home/autobuild && rm -f android-ndk.zip
+cd /home/autobuild && mv android-ndk-r12b ndk
+cd /home/autobuild && ls -al
+
+# Additional steps
+cd /home/autobuild/ndk && build/tools/make-standalone-toolchain.sh --toolchain=arm-linux-androideabi-4.9 --platform=android-21 --install-dir=/home/autobuild/ndkTC 
+cd /home/autobuild && ls -al && rm -rf SuperNET
+cd /home/autobuild && git clone https://github.com/jl777/SuperNET.git
+cd /home/autobuild && cp -a SuperNET/OSlibs/android/lib/lib* ndkTC/sysroot/usr/lib/
+cd /home/autobuild && cp -av SuperNET/OSlibs/android/include/openssl ndkTC/sysroot/usr/include/
+cd /home/autobuild && cp -av SuperNET/OSlibs/android/include/curl ndkTC/sysroot/usr/include/
+cd /home/autobuild && rm -rf SuperNET
